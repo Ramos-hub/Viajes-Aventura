455faab9533635a6c3a60ebf08462d6150f59131
Ramos-hub <20220349@ricaldone.edu.sv>
2025-03-21 00:25:32 -0600

    viajes

---
+++ Desktop/Diego Ramos/Calcular_raiz_cuadrada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_raiz_cuadrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, raiz;
+            Console.WriteLine("Ingrese el número de que desea saber la raíz");
+            num = double.Parse(Console.ReadLine());
+            raiz = Math.Sqrt(num);
+            Console.WriteLine("La raíz cuadrada del número es: {0}", raiz);
+            Console.ReadKey();
+        }
+    }
+}
